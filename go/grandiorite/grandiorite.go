b6b900edcef4f7b67267f44ef54b563b091c9966
Tieg Zaharia <tieg.zaharia@gmail.com>
2019-05-05 12:36:17 -0400

    Adds go

---
+++ go/grandiorite/grandiorite.go
@@ -0,0 +1,7 @@
+package grandiorite
+
+import "fmt"
+
+func HelloWorld () {
+	fmt.Println("Hello, world.")
+}
---
+++ go/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "grandiorite/tiegz/grandiorite/go"
+
+func main () {
+	HelloWorld()
+}
