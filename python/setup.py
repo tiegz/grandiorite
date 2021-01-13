affadef9a1ad12bdf6a726792a143e315f21b8e4
Tieg Zaharia <tieg@tidelift.com>
2021-01-13 14:04:08 -0500

    Set all versions to 1.0.1.

--- python/setup.py
+++ python/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="grandiorite",
-    version="1.0.0",
+    version="1.0.1",
     author="Tieg Zaharia",
     author_email="tieg.zaharia@gmail.com",
     description="Hello World in python.",
