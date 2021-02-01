6068e72b948e567b6f035220da5eb469760abcd7
Tieg Zaharia <tieg@tidelift.com>
2021-01-31 19:56:28 -0500

    Disable signing for Clojures deploy.

--- clojure/project.clj
+++ clojure/project.clj
@@ -1,4 +1,5 @@
-(defproject grandiorite "0.1.0"
+(defproject
+  grandiorite "0.1.0"
   :description "A simple hello world package"
   :url "https://github.com/tiegz/grandiorite/clojure/README.md"
   :license {:name "TODO"
@@ -6,5 +7,7 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojars.pjlegato/clansi "1.3.0"]]
   :main grandiorite.core
+  :aot [grandiorite.core]
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :deploy-repositories [["clojars" {:url "https://clojars.org/repo" :sign-releases false}]]
+)
