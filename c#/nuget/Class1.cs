2d0e09b51d9f7a04ff0f334f8939d257e54f5246
Tieg Zaharia <tieg@tidelift.com>
2024-05-31 13:12:06 -0700

    nuget 1.0.0

---
+++ c#/nuget/Class1.cs
@@ -0,0 +1,26 @@
+﻿namespace grandiorite;
+
+using Pastel;
+using System;
+
+public class Class1
+{
+  
+  static void Main(string[] args) {
+    Console.WriteLine(
+      "H".Pastel(System.Drawing.Color.Red) + 
+      "e".Pastel(System.Drawing.Color.Red) + 
+      "l".Pastel(System.Drawing.Color.DarkOrange) + 
+      "l".Pastel(System.Drawing.Color.Orange) + 
+      "o".Pastel(System.Drawing.Color.Yellow) + 
+      ",".Pastel(System.Drawing.Color.LightGoldenrodYellow) + 
+      " ".Pastel(System.Drawing.Color.Green) + 
+      "W".Pastel(System.Drawing.Color.LightGreen) + 
+      "o".Pastel(System.Drawing.Color.Blue) + 
+      "r".Pastel(System.Drawing.Color.LightBlue) + 
+      "l".Pastel(System.Drawing.Color.Lavender) + 
+      "d".Pastel(System.Drawing.Color.Purple) + 
+      "!".Pastel(System.Drawing.Color.White)
+    );
+  }
+}
---
+++ c#/nuget/README.md
@@ -0,0 +1,4 @@
+### Run
+
+`dotnet run`
+
